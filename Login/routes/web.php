ea5f9e9e79250ef91712bc618c0281af6e42796d
Rafid Redwan Khan <rafidredwankhan@gmail.com>
2023-08-19 14:24:46 +0600

    Laravel Relationships and Login Functionality

---
+++ Login/routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+if (Auth::check()) {
+
+    return "the user is logged in";
+}
+
+$name = 'admin';
+$password = "123";
+
+if (Auth::attempt(['name' => $name, 'password' => $password])) {
+
+    return redirect()->intended('/admin');
+}
+
+// Auth::logout();
